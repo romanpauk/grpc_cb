4f7584e37d5e04712a315d83e418bb1023c19a91
Roman Paukner <romanpauk@gmail.com>
2022-09-01 09:31:50 +0200

    initial project setup

---
+++ test/grpc_cb.cpp
@@ -0,0 +1,13 @@
+//
+// This file is part of smart_ptr project <https://github.com/romanpauk/grpc_cb>
+//
+// See LICENSE for license and copyright information
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <gtest/gtest.h>
+
+TEST(grpc_cb_test, test)
+{
+    
+}
